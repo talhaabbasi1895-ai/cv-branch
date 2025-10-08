c40ffa131a0208c5295e2f5f05c754bcacf95e16
talhaabbasi1895-ai <talhaabbasi1895@gmail.com>
2025-10-07 21:21:20 -0700

    Add files via upload

---
+++ CV.bat
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>List</title>
+</head>
+<body>
+    <p>
+         <strong></strong>  </p>
+        <!-- block quote (for long sentence) -->
+         <h3>OBJECTIVE</h3>
+         <blockquote>
+            <P>to obtain position where i can utilize my skills and knowledge to contribute to the success of the organization while growing professionally</P>
+         </blockquote>
+        <h3>WORK EXPPERIENCE </h3>
+         <blockquote>
+            <p>DARAZ INTERATIONNAL
+            </p>
+            <p>qcc department 2024-2025 designation qc operator</p>
+         </blockquote>
+         <!-- <q> fort short sentence -->
+            <h2>EXPERTISE SKILL</h2>
+            <P>Event management diploma from skill Development council </p>
+                
+            <!-- abbr -->
+
+            <p>Mr<abbr title="Talha abbasi">TB</abbr>  Student WMA</p>
+
+            <!-- dont't use-->
+
+            <p><acronym title=""></acronym>  </p>
+
+
+            
+
+          
+
+              <!--change content-->
+              <h3>EDUCATION</h3>
+              <p>Matriculation from science form karachi Board</p>
+              <p>intermediate with commerce from karachi</p>
+              <h3>CONTACT</h3>
+
+
+
+
+
+
+
+
+
+               <address>
+            
+                
+
+
+
+
+
+
+
+
+                
+             <a href="mailto">click for contact throung email</a>   
+              </p
+              
+              
+
+
+
+    </address>
+
+
+
+
+    
+
+</body>
+
+
+</html>
